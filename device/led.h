3c364cc06dbea48be44f29e2525e78fdc3780d17
Rajmund Szymanski <rajszym@o2.pl>
2017-08-18 15:35:23 +0200

    fix led

--- device/led.h
+++ device/led.h
@@ -5,7 +5,7 @@
 
 #include <stm8s.h>
 
-struct  __LEDs { uint8_t: 7; volatile uint8_t f: 1; };
+struct  __LEDs { volatile uint8_t f: 1; uint8_t: 7; };
 
 #define   LED (((struct __LEDs *)&(GPIOD->ODR))->f)
 
@@ -13,11 +13,11 @@ static inline void led_init( void )
 {
 	GPIOD->DDR |= 0x01;
 	GPIOD->CR1 |= 0x01;
-	LED = 1;
+	GPIOD->ODR |= 0x01;
 }
 
-static inline void led_set   ( void ) { LED  = 0; }
-static inline void led_clear ( void ) { LED  = 1; }
-static inline void led_toggle( void ) { LED ^= 1; }
+static inline void led_set   ( void ) { GPIOD->ODR &= 0xFE; }
+static inline void led_clear ( void ) { GPIOD->ODR |= 0x01; }
+static inline void led_toggle( void ) { GPIOD->ODR ^= 0x01; }
 
 #endif//__LED_H__
