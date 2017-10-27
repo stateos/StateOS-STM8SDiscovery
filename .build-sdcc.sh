b2ddaaf6bc6ad0811ba8d934e0504199e986fe2b
rajszym <rajszym@o2.pl>
2017-10-27 06:48:39 +0200

    travis-ci

--- .build-sdcc.sh
+++ .build-sdcc.sh
@@ -3,28 +3,28 @@
 set -e
 
 # install dependencies
-sudo    apt-get install -y curl subversion build-essential flex bison libboost-dev texinfo stx-btree-dev zip netcat gawk python2.7
+sudo apt-get install -y curl subversion build-essential flex bison libboost-dev texinfo stx-btree-dev zip netcat gawk python2.7
 # get the source code
-svn     checkout svn://svn.code.sf.net/p/sdcc/code/trunk
+svn checkout svn://svn.code.sf.net/p/sdcc/code/trunk
 # build and install
-cd      trunk/sdcc
-./configure                   \
-        --disable-mcs51-port  \
-        --disable-z80-port    \
-        --disable-z180-port   \
-        --disable-r2k-port    \
-        --disable-r3ka-port   \
-        --disable-gbz80-port  \
-        --disable-tlcs90-port \
-        --disable-ds390-port  \
-        --disable-ds400-port  \
-        --disable-pic14-port  \
-        --disable-pic16-port  \
-        --disable-hc08-port   \
-        --disable-s08-port    \
-        --disable-ucsim       \
-        --disable-non-free
+cd trunk/sdcc
+./configure                    \
+         --disable-mcs51-port  \
+         --disable-z80-port    \
+         --disable-z180-port   \
+         --disable-r2k-port    \
+         --disable-r3ka-port   \
+         --disable-gbz80-port  \
+         --disable-tlcs90-port \
+         --disable-ds390-port  \
+         --disable-ds400-port  \
+         --disable-pic14-port  \
+         --disable-pic16-port  \
+         --disable-hc08-port   \
+         --disable-s08-port    \
+         --disable-ucsim       \
+         --disable-non-free
 make -s
-make -s install
-cd      ../..
-rm -rf  trunk
+sudo make -s install
+cd ../..
+rm -rf trunk
